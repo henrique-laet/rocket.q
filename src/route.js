84d52ab49d82f246f62cf1d19b564a4dc9e458f6
henrique-laet <henriquebaslaet@hotmail.com>
2021-06-30 23:04:56 -0300

    Adicionando banco de dados e ajustes

--- src/route.js
+++ src/route.js
@@ -7,10 +7,11 @@ const route = express.Router()
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
 route.get('/creat-pass', (req, res) => res.render("index", {page: 'create-pass'}))
 
-route.get('/room/:room', (req, res) => res.render("room"))
+route.post('/create-room', RoomController.create)
+route.get('/room/:room', RoomController.open)
+route.post('/enterroom', RoomController.enter)
 
-// Formato que o formulario de dentro da modal tem que passar a informaçao
-route.post('/room/:room/:question/:action', QuestionController.index)
-route.post('/room/create-room', RoomController.create)
+route.post('/question/create/:room', QuestionController.create)
+route.post('/question/:room/:question/:action', QuestionController.index)
  
 module.exports = route 
